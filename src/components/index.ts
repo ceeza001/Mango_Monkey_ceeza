4c1f256b44edba66e7f522dc342458fcce417b7d
ceeza001 <mesalokubor@gmail.com>
2024-07-06 22:39:14 +0000

    First commit

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,4 @@
-export { default as Loader } from "./Loader";
 export { default as Topbar } from "./Topbar";
-export { default as AnimatedText } from "./AnimatedText";
 export { default as Hero } from "./Hero";
 export { default as Features } from "./Features";
 export { default as Anticipate } from "./Anticipate";
